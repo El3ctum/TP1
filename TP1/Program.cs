4f46eb953ac89eb0f1470932f421361c43626000
scarchess <davileamarcal198@gmail.com>
2024-03-11 00:17:45 -0300

    Adicionar arquivos de projeto.

---
+++ TP1/Program.cs
@@ -0,0 +1,80 @@
+﻿using TP1.models;
+
+namespace TP1
+{
+	public class Program
+	{
+		private static List<Pessoa> _pessoas = [];
+
+		public static void Main(string[] args)
+		{
+			// Cria uma instância da classe Pessoa
+			Pessoa pessoa1 = new Pessoa("João Silva", 31, "123.456.789-00");
+
+			// Apresenta a pessoa na tela
+			Console.WriteLine(pessoa1.ToString());
+
+			// Cria um menu em um laço infinito
+			while (true)
+			{
+				// Mostra as opções do menu
+				Console.WriteLine("------------------");
+				Console.WriteLine("Opções:");
+				Console.WriteLine("1 - Adicionar Pessoa");
+				Console.WriteLine("2 - Mostrar Pessoas");
+				Console.WriteLine("0 - Sair");
+				Console.WriteLine("------------------");
+
+				// Lê a opção do usuário
+				int opcao = int.Parse(Console.ReadLine());
+
+				switch (opcao)
+				{
+					case 1:
+						AdicionarPessoa();
+						break;
+					case 2:
+						MostrarPessoas();
+						break;
+					case 0:
+						Environment.Exit(0);
+						break;
+					default:
+						Console.WriteLine("Opção inválida!");
+						break;
+				}
+			}
+		}
+
+		private static void AdicionarPessoa()
+		{
+			// Lê os dados da pessoa
+			Console.WriteLine("Digite o nome:");
+			string nome = Console.ReadLine();
+
+			Console.WriteLine("Digite a idade:");
+			int idade = int.Parse(Console.ReadLine());
+
+			Console.WriteLine("Digite o CPF:");
+			string cpf = Console.ReadLine();
+
+			// Cria uma nova instância da classe Pessoa
+			Pessoa pessoa = new Pessoa(nome, idade, cpf);
+
+			// Adiciona a pessoa à lista
+			_pessoas.Add(pessoa);
+
+			Console.WriteLine("Pessoa adicionada com sucesso!");
+		}
+
+		private static void MostrarPessoas()
+		{
+			// Mostra todas as pessoas da lista
+			foreach (Pessoa pessoa in _pessoas)
+			{
+				Console.WriteLine(pessoa.ToString());
+			}
+		}
+	}
+
+}
---
+++ TP1/models/Pessoa.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace TP1.models
+{
+	public class Pessoa
+	{
+		private string _nome;
+		private int _idade;
+		private string _cpf;
+
+		public string Nome
+		{
+			get { return _nome; }
+			set
+			{
+				if (string.IsNullOrEmpty(value))
+				{
+					throw new ArgumentException("O nome não pode ser nulo ou vazio.");
+				}
+
+				_nome = value;
+			}
+		}
+
+		public int Idade
+		{
+			get { return _idade; }
+			set
+			{
+				if (_idade < 0 || _idade > 120)
+				{
+					throw new ArgumentException("A idade deve estar entre 0 e 120 anos.");
+				}
+				_idade = value;
+			}
+		}
+
+		public string CPF
+		{
+			get { return _cpf; }
+			set
+			{
+				if (value.Length < 11 || value.Length > 14)
+				{
+					throw new ArgumentException("O CPF deve ter 11 dígitos sem pontução ou 14 incluindo as pontuações.");
+				}
+
+				_cpf = value;
+			}
+		}
+
+		public Pessoa(string nome, int idade, string cpf)
+		{
+			Nome = nome;
+			Idade = idade;
+			CPF = cpf;
+		}
+
+		public override string ToString()
+		{
+			return $"Nome: {Nome}, Idade: {Idade}, CPF: {CPF}";
+		}
+
+	}
+
+
+}
